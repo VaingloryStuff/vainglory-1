5dcd231553e4600a4912f868f629dc03887b945f
kvahuja <k.v.ahuja@gmail.com>
2017-02-05 21:20:42 +0530

    home page setup

---
+++ resources/views/home.blade.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Vainglory - Advanced Vainglory Stats, Leaderboards, Player Profiles and Dashbaords</title>
+
+	<!-- Global stylesheets -->
+	<link href="assets/css/icons/icomoon/styles.css" rel="stylesheet" type="text/css">
+	<link href="assets/css/bootstrap.css" rel="stylesheet" type="text/css">
+	<link href="assets/css/core.css" rel="stylesheet" type="text/css">
+	<link href="assets/css/components.css" rel="stylesheet" type="text/css">
+	<link href="assets/css/colors.css" rel="stylesheet" type="text/css">
+	<!-- /global stylesheets -->
+
+	<!-- Core JS files -->
+	<script type="text/javascript" src="assets/js/plugins/loaders/pace.min.js"></script>
+	<script type="text/javascript" src="assets/js/core/libraries/jquery.min.js"></script>
+	<script type="text/javascript" src="assets/js/core/libraries/bootstrap.min.js"></script>
+	<script type="text/javascript" src="assets/js/plugins/loaders/blockui.min.js"></script>
+	<script type="text/javascript" src="assets/js/plugins/ui/nicescroll.min.js"></script>
+	<script type="text/javascript" src="assets/js/plugins/ui/drilldown.js"></script>
+	<!-- /core JS files -->
+
+
+	<!-- Theme JS files -->
+	<script type="text/javascript" src="assets/js/core/app.js"></script>
+
+	<script type="text/javascript" src="assets/js/plugins/ui/ripple.min.js"></script>
+	<!-- /theme JS files -->
+
+</head>
+
+<body>
+
+	<!-- Main navbar -->
+	<div class="navbar navbar-inverse bg-indigo">
+		<div class="navbar-header">
+			<a class="navbar-brand" href="index.html"><img src="assets/images/logo.png" alt=""></a>
+
+			<ul class="nav navbar-nav pull-right visible-xs-block">
+				<li><a data-toggle="collapse" data-target="#navbar-mobile"><i class="icon-tree5"></i></a></li>
+			</ul>
+		</div>
+
+		<div class="navbar-collapse collapse" id="navbar-mobile">
+			<ul class="nav navbar-nav navbar-right">
+				<li class="dropdown language-switch">
+					<a class="dropdown-toggle" data-toggle="dropdown">
+						<img src="assets/images/flags/gb.png" class="position-left" alt="">
+						English
+						<span class="caret"></span>
+					</a>
+
+					<ul class="dropdown-menu">
+						<li><a class="ukrainian"><img src="assets/images/flags/in.png" alt=""> Hindi</a></li>
+						<li><a class="english"><img src="assets/images/flags/gb.png" alt=""> English</a></li>
+					</ul>
+				</li>
+			</ul>
+		</div>
+	</div>
+	<!-- /main navbar -->
+
+
+	<!-- Page container -->
+	<div class="page-container">
+
+		<!-- Page content -->
+		<div class="page-content">
+
+			<!-- Main content -->
+			<div class="content-wrapper">
+
+				<!-- Search title -->
+				<div class="text-center content-group">
+					<div class="row">
+						<div class="col-lg-6 text-center">
+							<img class="logo-image" src="assets/images/logo.png" alt="">
+						</div>
+						<div class="col-lg-6 text-left">
+							<h1 class="main-title">Vain Stats</h1>
+						</div>
+					</div>
+					<h3 class="sub-title">Advanced Vainglory Stats</h3>
+					<h3 class="sub-sub-title">Coming Soon!!</h3>
+				</div>
+				<!-- /search title -->
+
+
+				<!-- Error content -->
+				<div class="row">
+					<div class="col-lg-4 col-lg-offset-4 col-sm-6 col-sm-offset-3">
+						<form action="#" class="main-search panel panel-body">
+							<div class="form-group has-feedback">
+								<input type="text" class="form-control input-lg" placeholder="Search...">
+								<div class="form-control-feedback">
+									<i class="icon-search4 text-size-large text-muted"></i>
+								</div>
+							</div>
+
+							<div class="text-center">
+								<a href="#" class="btn bg-pink-400"><i class="icon-circle-left2 position-left"></i> Go</a>
+							</div>
+						</form>
+					</div>
+				</div>
+				<!-- /error wrapper -->
+
+			</div>
+			<!-- /main content -->
+
+		</div>
+		<!-- /page content -->
+
+	</div>
+	<!-- /page container -->
+
+
+	<!-- Footer -->
+	<div class="footer text-muted text-center">
+	</div>
+	<!-- /footer -->
+
+</body>
+</html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,31 @@ const { mix } = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+
+ mix.copy('resources/assets/css/icons/icomoon/styles.css', 'public/assets/css/icons/icomoon/styles.css');
+ mix.copy('resources/assets/css/bootstrap.css', 'public/assets/css/bootstrap.css');
+ mix.copy('resources/assets/css/core.css', 'public/assets/css/core.css');
+ mix.copy('resources/assets/css/components.css', 'public/assets/css/components.css');
+ mix.copy('resources/assets/css/colors.css', 'public/assets/css/colors.css');
+
+ mix.copy('resources/assets/js/plugins/loaders/pace.min.js', 'public/assets/js/plugins/loaders/pace.min.js');
+ mix.copy('resources/assets/js/core/libraries/jquery.min.js', 'public/assets/js/core/libraries/jquery.min.js');
+ mix.copy('resources/assets/js/core/libraries/bootstrap.min.js', 'public/assets/js/core/libraries/bootstrap.min.js');
+ mix.copy('resources/assets/js/plugins/loaders/blockui.min.js', 'public/assets/js/plugins/loaders/blockui.min.js');
+ mix.copy('resources/assets/js/plugins/ui/nicescroll.min.js', 'public/assets/js/plugins/ui/nicescroll.min.js');
+ mix.copy('resources/assets/js/plugins/ui/drilldown.js', 'public/assets/js/plugins/ui/drilldown.js');
+
+ mix.copy('resources/assets/js/core/app.js', 'public/assets/js/core/app.js');
+ mix.copy('resources/assets/js/plugins/ui/ripple.min.js', 'public/assets/js/plugins/ui/ripple.min.js');
+
+
+ mix.copy('resources/assets/images/logo_dark.png', 'public/assets/images/logo_dark.png');
+ mix.copy('resources/assets/images/logo.png', 'public/assets/images/logo.png');
+ mix.copy('resources/assets/images/placeholder.jpg', 'public/assets/images/placeholder.jpg');
+ mix.copy('resources/assets/images/flags/gb.png', 'public/assets/images/flags/gb.png');
+ mix.copy('resources/assets/images/flags/in.png', 'public/assets/images/flags/in.png');
+
+ mix.copy('resources/assets/css/icons/icomoon/fonts/icomoon.eot', 'public/assets/css/icons/icomoon/fonts/icomoon.eot');
+ mix.copy('resources/assets/css/icons/icomoon/fonts/icomoon.svg', 'public/assets/css/icons/icomoon/fonts/icomoon.svg');
+ mix.copy('resources/assets/css/icons/icomoon/fonts/icomoon.ttf', 'public/assets/css/icons/icomoon/fonts/icomoon.ttf');
+ mix.copy('resources/assets/css/icons/icomoon/fonts/icomoon.woff', 'public/assets/css/icons/icomoon/fonts/icomoon.woff');
